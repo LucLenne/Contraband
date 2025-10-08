97cfd273472d3cf1770e1b6862ddf044a04326d1
LucLenne <luc.lenne@outlook.fr>
2025-10-08 15:33:11 +0200

    [ADD : Baron / Start TutoManager]

--- Cantraband/Assets/Contraband/Code/Script/Tuto/Baron.cs
+++ Cantraband/Assets/Contraband/Code/Script/Tuto/Baron.cs
@@ -1,10 +1,7 @@
-using NUnit.Framework;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
-using UnityEngine.Localization.Tables;
 
 public class Baron : MonoBehaviour
 {
@@ -14,27 +11,41 @@ public class Baron : MonoBehaviour
     [SerializeField] private GameObject _baron;
     
     [Header("Data"),SerializeField] private OrderSpeech _speechBaron;
-    
-    
-    private void DisplaySpeech(Speech speech, int indexOrder)
+
+    private void Start()
+    {
+        SpeechBaron(0);
+    }
+
+    private void DisplaySpeech(Speech speech)
     {
         _textSpeech.text = LocalizationSettings.StringDatabase.GetLocalizedString(_tableName, speech.id);
     }
 
-
-    private async Task ShowSpeech()
+    public async void SpeechBaron( int p)
     {
-
+        if (p > 3 || p < 0) Debug.LogError("Wrong part : " + p + ". Choose Between 0 and 3");
+        if (_speechBaron.listSpeech[p].Count == 0) return;
+        _baron.SetActive(true);
+        foreach (Speech speech in _speechBaron.listSpeech[p]) 
+        {
+            DisplaySpeech(speech);
+            await Tasks.WaitSeconds(speech.time);
+        }
+        UnloadText();
+        _baron.SetActive(false);
     }
 
-
-
+    private void UnloadText()
+    {
+        _textSpeech.text = "";
+    }
 }
 
 [System.Serializable]
 public class Speech
 {
-    public float time;
+    public int time;
     public string id;
 }
 
@@ -45,4 +56,11 @@ public class OrderSpeech
     public List<Speech> secondPart;
     public List<Speech> thirdPart;
     public List<Speech> fourthPart;
+
+    public List<List<Speech>> listSpeech;
+
+    public OrderSpeech() 
+    { 
+        listSpeech = new() { firstPart, secondPart, thirdPart, fourthPart };
+    }
 }
--- Cantraband/Assets/Contraband/Code/Script/Tuto/TutoManager.cs
+++ Cantraband/Assets/Contraband/Code/Script/Tuto/TutoManager.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TutoManager : MonoBehaviour
 {
-    
+    [SerializeField] private Baron _baron;
+
+    [Header("3 Clients"), SerializeField] private List<Client> _listClient;
+
+    private void OnEnable()
+    {
+        InputManager.Instance.OnReadCard += CheckCard;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnReadCard -= CheckCard;
+    }
+
+    void CheckCard(int id)
+    {
+        
+    }
+
+
 }
