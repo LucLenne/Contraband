973ba25b397fcc2ab349ffbce2b6b32fd377c4f1
BVerstaen <bverstaen04@gmail.com>
2025-10-10 12:10:01 +0200

    [UPDATE] game over spawn scene

--- Cantraband/Assets/Contraband/Code/Script/UI/GameOver/GameOverScene.cs
+++ Cantraband/Assets/Contraband/Code/Script/UI/GameOver/GameOverScene.cs
@@ -34,17 +34,16 @@ public class GameOverScene : MonoBehaviour
         if (LevelManager.Instance.GameCardsGiven.Count != LevelManager.Instance.PointsAwarded.Count)
             Debug.LogError("number of game cards given isn't the same as poitns awarded");
 
-        for (int i = 0; i < LevelManager.Instance.PointsAwarded.Count; i++)
+        float _numberOfCartridges = LevelManager.Instance.GameCardsGiven.Count;
+        for (int i = 0; i < LevelManager.Instance.score; i++)
         {
             //Spawn game cards
-            if(i < LevelManager.Instance.GameCardsGiven.Count)
-            {
-                GameObject prefabToSpawn = LevelManager.Instance.GameCardsGiven[i].MeshPrefab;
-                Instantiate(prefabToSpawn, _spawnPoint.transform.position, Random.rotation);
-            }
+            int index = (int)(i % _numberOfCartridges);
+            GameObject prefabToSpawn = LevelManager.Instance.GameCardsGiven[index].MeshPrefab;
+            Instantiate(prefabToSpawn, _spawnPoint.transform.position, Random.rotation);
 
             //Add score to text
-            _currentScore += LevelManager.Instance.PointsAwarded[i];
+            _currentScore++;
             _scoreText.text = SCORE_TEXT + SCORE_ANIM + _currentScore;
 
             yield return new WaitForSeconds(_delayBTWspawns);
