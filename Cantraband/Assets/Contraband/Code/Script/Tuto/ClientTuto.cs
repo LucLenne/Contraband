afe159cf74bdb341da97f8692ad04e2a5093371e
LucLenne <luc.lenne@outlook.fr>
2025-10-13 11:53:18 +0200

    [UPDATE : Tuto / GameScene]

--- Cantraband/Assets/Contraband/Code/Script/Tuto/ClientTuto.cs
+++ Cantraband/Assets/Contraband/Code/Script/Tuto/ClientTuto.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class ClientTuto : MonoBehaviour
 {
-    [Header("UI"), SerializeField] private Image _imagePatience;
+    [Header("UI")]
+    [SerializeField] private Image _sliderImage;
+    [SerializeField] private Gradient _sliderGradient;
 
     [Header("Gameplay"), SerializeField] private int _timePatience = 10;
     public bool activeTimer;
+
+    [Header("Timer slider")]
+    [SerializeField] private RectTransform _timerRectTransform;
+    [SerializeField] private float _timerAnimMaxSpeed;
+    [SerializeField] private AnimationCurve _timerAnimCurve;
 
     [Header("References"), SerializeField] private GameObject _sliderGO;
     [SerializeField] private GameObject _hintImagePrefab;
@@ -28,6 +35,8 @@ public class ClientTuto : MonoBehaviour
     private void LaunchGameOverAnim() => _animator.SetTrigger(ANIMATION_GAME_OVER_NAME);
     public void LaunchTransferDoneAnim() => _animator.SetTrigger(ANIMATION_TRANSFER_DONE_NAME);
     public void LaunchOutOfPatience() => _animator.SetTrigger(ANIMATION_OUT_OF_PATIENCE);
+
+
 
     public void DestroyObject()
     {
@@ -78,24 +87,35 @@ public class ClientTuto : MonoBehaviour
     private void ActiveTimer()
     {
         _sliderGO.SetActive(true);
-        StartCoroutine(StartTimerPatience());
+        StartCoroutine(StartTimerAsync());
     }
 
-    private IEnumerator StartTimerPatience()
+    private IEnumerator StartTimerAsync()
     {
         float timeLeft = _timePatience;
-        _imagePatience.fillAmount = timeLeft;
+        _sliderImage.fillAmount = timeLeft;
 
+        Vector3 baseScale = _timerRectTransform.localScale;
+        _timerRectTransform.gameObject.SetActive(true);
         while (timeLeft > 0f)
         {
-            yield return null; // équivalent de Task.Yield() dans Unity Coroutine
+            yield return null;
 
             timeLeft -= Time.deltaTime;
-            _imagePatience.fillAmount = timeLeft;
-        }
 
-        _imagePatience.fillAmount = 0;
-        TutoManager.Instance.clientEndPatience?.Invoke();
+            float normalizedTime = 1 - (timeLeft / _timePatience); // 0 - 1
+            float currentSpeed = _timerAnimCurve.Evaluate(normalizedTime) * _timerAnimMaxSpeed;
+            float scaleFactor = 1f + Mathf.Sin(Time.time * currentSpeed * Mathf.PI) * 0.1f;
+
+            _timerRectTransform.localScale = baseScale * scaleFactor;
+            _sliderImage.color = _sliderGradient.Evaluate(normalizedTime);
+            _sliderImage.fillAmount = 1 - normalizedTime;
+        }
+        _timerRectTransform.localScale = baseScale;
+        _timerRectTransform.gameObject.SetActive(false);
+
+        _sliderImage.fillAmount = 0f;
+        LevelManager.Instance.StartClientNoMorePatience();
     }
 
 }
