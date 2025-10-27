8ea1197afc698603b440ba7a82b5d8607b73bfbb
BVerstaen <bverstaen04@gmail.com>
2025-10-27 16:41:51 +0100

    [ADD] Implement LaunchTriggerCameraPatrol

--- Cantraband/Assets/Contraband/Code/Script/Popup/PopUpProxi.cs
+++ Cantraband/Assets/Contraband/Code/Script/Popup/PopUpProxi.cs
@@ -12,6 +12,7 @@ public class PopUpProxi : MonoBehaviour
     public void LaunchTriggerAnimation(string triggerName) => _popUpScript.LaunchTrigger(triggerName);
     public void LaunchTriggerPolicePatrol(string triggerName) => _popUpScript.LaunchTriggerPolicePatrol(triggerName);
     public void LaunchTriggerFakeCameraAnimation(string triggerName) => _popUpScript.LaunchTriggerFakeCamera(triggerName);
+    public void LaunchTriggerCameraPatrol() => _popUpScript.LaunchCameraPatrol();
 
     public void LaunchGameOver()
     {
