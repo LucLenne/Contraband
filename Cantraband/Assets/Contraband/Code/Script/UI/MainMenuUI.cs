c1375b4abcdf0c3252d74725db0dc97bad344e6e
LucLenne <luc.lenne@outlook.fr>
2026-03-01 21:30:00 +0100

    [ADD] Scene Coat

--- Cantraband/Assets/Contraband/Code/Script/UI/MainMenuUI.cs
+++ Cantraband/Assets/Contraband/Code/Script/UI/MainMenuUI.cs
@@ -16,7 +16,7 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private string _tagCreditsStartCard;
     [SerializeField] private string _tagCreditsClavierDebug;
 
-    private const string NAME_TUTO_LEVEL = "Tuto";
+    private const string NAME_COAT_TUTO_LEVEL = "Coat Tuto";
     private const string NAME_GAME_LEVEL = "Game";
     private const string NAME_CREDITS_LEVEL = "Credits";
 
@@ -38,14 +38,13 @@ public class MainMenuUI : MonoBehaviour
     void CheckChard(string tag)
     {
         if (tag == _tagTutoStartCard || tag == _tagTutoClavierDebug)
-            StartTuto();
+            StartCoatTuto();
         else if (tag == _tagGameStartCard || tag == _tagGameClavierDebug)
             StartGame();
         else if (tag == _tagCreditsStartCard || tag == _tagCreditsClavierDebug)
             StartCredits();
     }
-
-    private void StartTuto() => LoadingManager.Instance.LoadScene(NAME_TUTO_LEVEL, true);
+    private void StartCoatTuto() => LoadingManager.Instance.LoadScene(NAME_COAT_TUTO_LEVEL, true);
     private void StartGame() => LoadingManager.Instance.LoadScene(NAME_GAME_LEVEL, true);
     private void StartCredits() => LoadingManager.Instance.LoadScene(NAME_CREDITS_LEVEL, true);
 }
